734dcb7646ed065b62510545906645f41a791677
Timur Badretdinov <destinerx@gmail.com>
2021-10-06 11:29:06 +0300

    Extract Multicall metadata into separate file

--- src/provider.ts
+++ src/provider.ts
@@ -2,6 +2,7 @@ import { BaseProvider } from '@ethersproject/providers';
 
 import { Call, all as callAll, tryAll as callTryAll } from './call';
 import { getEthBalance } from './calls';
+import { getMulticall, getMulticall2 } from './multicall';
 
 const DEFAULT_CHAIN_ID = 1;
 
@@ -70,44 +71,4 @@ export default class Provider {
 		const provider = this.provider as BaseProvider;
 		return await callTryAll(provider, this.multicall2Address, calls, block);
 	}
-}
-
-function getMulticall(chainId: number): string {
-	const addressMap: Record<number, string> = {
-		1: '0xeefba1e63905ef1d7acba5a8513c70307c1ce441',
-		3: '0x53c43764255c17bd724f74c4ef150724ac50a3ed',
-		4: '0x42ad527de7d4e9d9d011ac45b31d8551f8fe9821',
-		5: '0x77dca2c955b15e9de4dbbcf1246b4b85b651e50e',
-		10: '0xe295ad71242373c37c5fda7b57f26f9ea1088afe',
-		42: '0x2cc8688c5f75e365aaeeb4ea8d6a480405a48d2a',
-		56: '0xe21a5b299756ee452a6a871ff29852862fc99be9',
-		100: '0xb5b692a88bdfc81ca69dcb1d924f59f0413a602a',
-		108: '0xfce4609743e17d349b7e5f478a7a9a6cfa3c808c',
-		128: '0x56171094a15b8cac4314c0f8930100b939503bd9',
-		137: '0x35e4aa226ce52e1e59e5e5ec24766007bcbe2e7d',
-		250: '0xc04d660976c923ddba750341fe5923e47900cf24',
-		321: '0x543528e13eac69206e87334cca971503a552438b',
-		820: '0x8ba3d23241c7044be703afaf2a728fdbc16f5f6f',
-		1337: '0x77dca2c955b15e9de4dbbcf1246b4b85b651e50e',
-		4689: '0x0e14ded9e7965c6446df2e5c528dd1b4e3b4640f',
-		42161: '0x10126ceb60954bc35049f24e819a380c505f8a0f',
-		43114: '0xE8eeDd99baC03871CF123E76cE90bA179Df94351',
-		80001: '0x08411add0b5aa8ee47563b146743c13b3556c9cc',
-		1313161554: '0xa48c67d1c60b8187ecb7c549e8a670419d356994',
-		1666600000: '0xfe4980f62d708c2a84d3929859ea226340759320',
-	};
-	return addressMap[chainId];
-}
-
-function getMulticall2(chainId: number): string {
-	const addressMap: Record<number, string> = {
-		1: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
-		4: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
-		5: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
-		42: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
-		56: '0x4c6bb7c24b6f3dfdfb548e54b7c5ea4cb52a0069',
-		100: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
-		137: '0xf43a7be1b284aa908cdfed8b3e286961956b4d2c',
-	};
-	return addressMap[chainId];
 }
