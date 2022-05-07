608a19402358d91196dd0287d6f607903b3a3334
Timur Badretdinov <destinerx@gmail.com>
2022-05-07 20:01:20 +0400

    Migrate to abi-coder v3

--- src/abi.ts
+++ src/abi.ts
@@ -10,29 +10,37 @@ class Abi {
     jsonInputs: JsonFragmentType[],
     params: Params,
   ): string {
+    const inputs = backfillParamNames(jsonInputs);
     const abi = [
       {
         type: 'function',
         name,
-        inputs: jsonInputs,
+        inputs,
       },
     ];
     const coder = new Coder(abi);
-    return coder.encodeFunction(name, params);
+    const valueMap = Object.fromEntries(
+      inputs.map((input, index) => [input.name, params[index]]),
+    );
+    return coder.encodeFunction(name, valueMap);
   }
 
   static encodeConstructor(
     jsonInputs: JsonFragmentType[],
     params: Params,
   ): string {
+    const inputs = backfillParamNames(jsonInputs);
     const abi = [
       {
         type: 'constructor',
-        inputs: jsonInputs,
+        inputs,
       },
     ];
     const coder = new Coder(abi);
-    return coder.encodeConstructor(params);
+    const valueMap = Object.fromEntries(
+      inputs.map((input, index) => [input.name, params[index]]),
+    );
+    return coder.encodeConstructor(valueMap);
   }
 
   static decode(
@@ -40,17 +48,47 @@ class Abi {
     jsonOutputs: JsonFragmentType[],
     data: string,
   ): Result {
+    const outputs = backfillParamNames(jsonOutputs);
     const abi = [
       {
         type: 'function',
         name,
-        outputs: jsonOutputs,
+        outputs,
       },
     ];
     const coder = new Coder(abi);
-    const output = coder.decodeFunctionOutput(name, data);
-    return output.values;
+
+    const functionOutput = coder.decodeFunctionOutput(name, data);
+    return outputs.map(
+      (output) => functionOutput.values[output.name || ''],
+    );
   }
+}
+
+// ABI doesn't enforce to specify param names
+// However, abi-coder requires names to parse the params.
+// Therefore, we "patch" the ABI by assigning a unique param names.
+function backfillParamNames(jsonParams: JsonFragmentType[]): JsonFragmentType[] {
+  const names = new Set(...jsonParams.map((param) => param.name));
+  return jsonParams.map((param) => {
+    const { name: originalName, indexed, type, components } = param;
+    const name = originalName ? originalName : generateUniqueName(names);
+    names.add(name);
+    return {
+      name,
+      indexed,
+      type,
+      components,
+    };
+  });
+}
+
+function generateUniqueName(names: Set<string>): string {
+  let i = 0;
+  while (names.has(i.toString())) {
+    i++;
+  }
+  return i.toString();
 }
 
 export { Params };
