c802e99de47f760314c48323073cf8748559abfa
Timur Badretdinov <destinerx@gmail.com>
2023-03-11 15:22:02 +0400

    Fix param order for Provider constructor in tests

--- test/provider.test.ts
+++ test/provider.test.ts
@@ -13,7 +13,7 @@ describe('Provider', () => {
   test('throws if Multicall is not available', async () => {
     const errorMessage = 'Multicall contract is not available on this network.';
     const fakeProvider = new FakeProvider(0);
-    const provider = new Provider(fakeProvider, 0);
+    const provider = new Provider(0, fakeProvider);
     expect(() => provider.getEthBalance('')).toThrow(errorMessage);
     expect(provider.all([])).rejects.toThrow();
     expect(provider.tryAll([])).rejects.toThrow();
